473f4b6d281735a65a4933373c96607466435b7d
Leonardo Perdomo <leonardop763@gmail.com>
2022-04-26 22:42:11 -0400

    All the functions of the triangle were implemented, the logic was change to SPA with javascript

--- figures.js
+++ figures.js
@@ -1,43 +1,205 @@
-import { squareArea, squarePerimeter, trianglePerimeter, triangleArea, circleArea, circlePerimeter } from './modules/geometry.js';
+//Import the function from geometry.js module
+import {
+    squareArea, squarePerimeter,
+    equilateralTriangleArea, circleArea,
+    circlePerimeter, isocelesTriangleHeight,
+    equilateralTriangleHeight, isocelesTriangleArea,
+    isocelesTrianglePerimeter, equilateralTrianglePerimeter
+} from './modules/geometry.js';
 
+//The elements of the modal are gotten
 const modal = document.getElementById("modal");
 const closeModal = document.getElementById("close-modal");
 
-
-const triangleContainer = document.getElementById("triangle-container");
-const squareContainer = document.getElementById("square-container");
-const circleContainer = document.getElementById("circle-container");
+//The buttons are gotten 
 const squareButton = document.getElementById("square-button");
 const triangleButton = document.getElementById("triangle-button");
 const circleButton = document.getElementById("circle-button");
 
+var countOfActivation = 0;
+
+//Clicking the square button activates the section to perform square operations.
 const activateSquareForm = () => {
-    squareContainer.className = "flex-center";
-    triangleContainer.className = "hide-card";
-    circleContainer.className = "hide-card";
     squareButton.className = "option option-selected";
     triangleButton.className = "option option-noselected";
     circleButton.className = "option option-noselected";
+
+    document.getElementById("figure-container").innerHTML =
+        `<div class="card">
+        <h2>Area and Perimeter of a square</h2>
+        <form class="flex-column" action="">
+            <label class="form-labels" for="side">
+                Enter a side value of the square:
+            </label>
+            <input class="form-inputs" type="number" name="side" id="side" required />
+            <button class="button buttons button_sub" type="button" onClick="calculateSquarePerimeter()" name="perimeter">
+                Get the perimeter
+            </button>
+            <button class="button buttons button_sub" type="button" onClick="calculateSquareArea()" name="area">
+                Get the area
+            </button>
+        </form>
+    </div>`;
 }
 
-const activateTriangleForm = () => {
-    triangleContainer.className = "flex-center";
-    squareContainer.className = "hide-card";
-    circleContainer.className = "hide-card";
-    triangleButton.className = "option option-selected";
-    squareButton.className = "option option-noselected";
-    circleButton.className = "option option-noselected";
-}
-
+//Clicking the circle button activates the section to perform square operations.
 const activateCircleForm = () => {
-    circleContainer.className = "flex-center";
-    triangleContainer.className = "hide-card";
-    squareContainer.className = "hide-card";
     circleButton.className = "option option-selected";
     triangleButton.className = "option option-noselected";
     squareButton.className = "option option-noselected";
+
+    document.getElementById("figure-container").innerHTML =
+        `<div class="card">
+            <h2>Area and Perimeter of a circle</h2>
+            <form class="flex-column" action="">
+                <label class="form-labels" for="radius">
+                    Enter the radius of the circle:
+                </label>
+                <input class="form-inputs" type="number" name="radius" id="radius" required />
+                <button class="button buttons button_sub" type="button" onClick="calculateCirclePerimeter()"
+                    name="perimeter">
+                    Get the perimeter
+                </button>
+                <button class="button buttons button_sub" type="button" onClick="calculateCircleArea()" name="area">
+                    Get the area
+                </button>
+            </form>
+        </div>`;
 }
 
+//Clicking the triangle button activates the section to perform square operations.
+const activateTriangleForm = () => {
+    triangleButton.className = "option option-selected";
+    squareButton.className = "option option-noselected";
+    circleButton.className = "option option-noselected";
+
+    document.getElementById("figure-container").innerHTML =
+        `<div class="card">
+            <h2>Area and Perimeter and Heigth of a triangle</h2>
+            <form class="flex-column" action="" id="triangle-form">
+                <label for="triangle-type" class="form-labels">
+                    Select the type of triangle:
+                </label>
+                <select class="form-inputs" name="triangle-type" id="triangle-type" required onChange="activateTriangleCalculation()">
+                    <option value="">Please select a type of triangle</option>
+                    <option value="isosceles">Isosceles</option>
+                    <option value="equilateral">Equilateral</option>
+                </select>
+            </form>
+        </div>`;
+}
+
+//When choosing an option from the drop-down list, a list is activated to choose the operation to calculate
+const activateTriangleCalculation = () => {
+    let triangleType = document.getElementById("triangle-type").value;
+    if (countOfActivation === 0) {
+        document.getElementById("triangle-form").innerHTML +=
+            `<label for="calculation-type" class="form-labels">
+                Select the type of calculation:
+            </label>
+            <select class="form-inputs" name="calculation-type" id="calculation-type" required onChange="activateTriangleInputs()">                <option value="">Please select a type of calculation</option>
+                <option value="area">Area</option>
+                <option value="perimeter">Perimeter</option>
+                <option value="height">Height</option>
+            </select>
+            <div class="flex-column" id="triangle-inputs">
+            </div>`;
+        countOfActivation = 1;
+    }
+    document.getElementById("triangle-type").value = triangleType;
+}
+
+//When choosing an option, the inputs are activated to enter the data of the triangle
+const activateTriangleInputs = () => {
+    //It is necessary to obtain the values ​​of the dropdown lists to show them again and make decisions with conditionals based on them
+    let triangleType = document.getElementById("triangle-type").value;
+    let calculationType = document.getElementById("calculation-type").value;
+    if (triangleType === "isosceles") {
+        if (calculationType === "area") {
+            document.getElementById("triangle-inputs").innerHTML =
+                `<label for="heigth" class="form-labels">
+                    Enter the heigth of the triangle:
+                </label>
+                <input class="form-inputs" type="number" name="heigth" id="heigth" required />
+                <label for="base" class="form-labels">
+                    Enter the base of the triangle:
+                </label>
+                <input class="form-inputs" type="number" name="base" id="base" required />
+                <button class="button buttons button_sub" type="button" onClick="calculateTriangleArea()" name="area">
+                    Get the area
+                </button>`;
+        } else if (calculationType === "perimeter") {
+            document.getElementById("triangle-inputs").innerHTML =
+                `<label for="side1" class="form-labels">
+                    Enter the first side of the triangle:
+                </label>
+                <input class="form-inputs" type="number" name="side1" id="side1" required />
+                <label for="side2" class="form-labels">
+                    Enter the second side of the triangle:
+                </label>
+                <input class="form-inputs" type="number" name="side2" id="side2" required />
+                <label for="base" class="form-labels">
+                    Enter the base of the triangle:
+                </label>
+                <input class="form-inputs" type="number" name="base" id="base" required />
+                <button class="button buttons button_sub" type="button" onClick="calculateTrianglePerimeter()" name="perimeter">
+                    Get the perimeter
+                </button>`;
+        } else if (calculationType === "height") {
+            document.getElementById("triangle-inputs").innerHTML =
+                `<label for="base" class="form-labels">
+                    Enter the base of the triangle:
+                </label>
+                <input class="form-inputs" type="number" name="base" id="base" required />
+                <label for="side1" class="form-labels">
+                    Enter the first side of the triangle:
+                </label>
+                <input class="form-inputs" type="number" name="side1" id="side1" required />
+                <label for="side2" class="form-labels">
+                    Enter the second side of the triangle:
+                </label>
+                <input class="form-inputs" type="number" name="side2" id="side2" required />
+                <button class="button buttons button_sub" type="button" onClick="calculateTriangleHeight()" name="height">
+                    Get the height
+                </button>`;
+        }
+    } else if (triangleType === "equilateral") {
+        if (calculationType === "area") {
+            document.getElementById("triangle-inputs").innerHTML =
+                `<label for="base" class="form-labels">
+                    Enter the base of the triangle:
+                </label>
+                <input class="form-inputs" type="number" name="base" id="base" required /> 
+                <label for="heigth" class="form-labels">
+                    Enter the heigth of the triangle:
+                </label>
+                <input class="form-inputs" type="number" name="heigth" id="heigth" required />
+                <button class="button buttons button_sub" type="button" onClick="calculateTriangleArea()" name="area">
+                    Get the area
+                </button>`;
+        } else if (calculationType === "perimeter") {
+            document.getElementById("triangle-inputs").innerHTML =
+                `<label for="side" class="form-labels">
+                    Enter any side of the triangle:
+                </label>
+                <input class="form-inputs" type="number" name="side" id="side" required />
+                <button class="button buttons button_sub" type="button" onClick="calculateTrianglePerimeter()" name="perimeter">
+                    Get the perimeter
+                </button>`;
+        } else if (calculationType === "height") {
+            document.getElementById("triangle-inputs").innerHTML =
+                `<label for="side1" class="form-labels">
+                    Enter any side of the triangle:
+                </label>
+                <input class="form-inputs" type="number" name="side1" id="side1" required />
+                <button class="button buttons button_sub" type="button" onClick="calculateTriangleHeight()" name="height">
+                    Get the height
+                </button>`;
+        }
+    }
+    document.getElementById("triangle-type").value = triangleType;
+    document.getElementById("calculation-type").value = calculationType;
+}
 
 //Arrow function to calculate the area of the square
 const calculateSquareArea = () => {
@@ -53,7 +215,7 @@ const calculateSquareArea = () => {
     }
     //The modal is displayed
     modal.showModal();
-};
+}
 
 //Arrow function to calculate the perimeter of the square
 const calculateSquarePerimeter = () => {
@@ -63,37 +225,6 @@ const calculateSquarePerimeter = () => {
         document.getElementById("modal-content").innerHTML = `The perimeter of the square is ${mySquarePerimeter}.`;
     } else {
         document.getElementById("modal-content").innerHTML = "Please enter a number.";
-    }
-    modal.showModal();
-}
-
-const calculateTrianglePerimeter = () => {
-    let side1 = Number(document.getElementById('side1').value);
-    let side2 = Number(document.getElementById('side2').value);
-    let base = Number(document.getElementById('base').value);
-
-    if (side1 == 0 || side2 == 0 || base == 0) {
-        document.getElementById("modal-content").innerHTML = "Every side of the triangle must be greater than 0.";
-    } else if (side1 !== "" && side2 !== "" && base !== "") {
-        let myTrianglePerimeter = trianglePerimeter(side1, side2, base);
-        document.getElementById("modal-content").innerHTML = `The perimeter of the triangle is ${myTrianglePerimeter}.`;
-    } else {
-        document.getElementById("modal-content").innerHTML = "Please enter a number.";
-    }
-    modal.showModal();
-}
-
-const calculateTriangleArea = () => {
-    let base = document.getElementById('base').value;
-    let height = document.getElementById('height').value;
-
-    if (base == 0 || height == 0) {
-        document.getElementById("modal-content").innerHTML = "Every side of the triangle must be greater than 0.";
-    } else if (base !== "" && height !== "") {
-        let myTriangleArea = triangleArea(base, height);
-        document.getElementById("modal-content").innerHTML = `The area of the triangle is ${myTriangleArea}.`;
-    } else {
-        document.getElementById("modal-content").innerHTML = "Please enter the base and height of the triangle.";
     }
     modal.showModal();
 }
@@ -124,19 +255,79 @@ const calculateCirclePerimeter = () => {
     modal.showModal();
 }
 
+const calculateTriangleHeight = () => {
+    let triangleType = document.getElementById("triangle-type").value;
+
+    if (triangleType === "isosceles") {
+        let side1 = document.getElementById('side1').value;
+        let side2 = document.getElementById('side2').value;
+        let base = document.getElementById('base').value;
+        let myIsocelesTriangleHeight = isocelesTriangleHeight(base, side1, side2);
+        document.getElementById("modal-content").innerHTML = myIsocelesTriangleHeight;
+    } else if (triangleType === "equilateral") {
+        let side1 = document.getElementById('side1').value;
+        let myEquilateralTriangleHeight = equilateralTriangleHeight(side1);
+        document.getElementById("modal-content").innerHTML = `The heigth of the triangle is ${myEquilateralTriangleHeight}.`;
+    }
+    modal.showModal();
+}
+
+const calculateTriangleArea = () => {
+    let triangleType = document.getElementById("triangle-type").value;
+    let base = document.getElementById('base').value;
+    let heigth = document.getElementById('heigth').value;
+
+    if (triangleType === "isosceles") {
+
+        let myIsocelesTriangleArea = isocelesTriangleArea(base, heigth);
+        document.getElementById("modal-content").innerHTML = `The area of the triangle is ${myIsocelesTriangleArea}.`;
+    } else if (triangleType === "equilateral") {
+        let myEquilateralTriangleArea = equilateralTriangleArea(base, heigth);
+        document.getElementById("modal-content").innerHTML = `The area of the triangle is ${myEquilateralTriangleArea}.`;
+    }
+    modal.showModal();
+}
+
+const calculateTrianglePerimeter = () => {
+    let triangleType = document.getElementById("triangle-type").value;
+
+    if (triangleType === "isosceles") {
+        let side1 = document.getElementById('side1').value;
+        let side2 = document.getElementById('side2').value;
+        let base = document.getElementById('base').value;
+        let myIsocelesTrianglePerimeter = isocelesTrianglePerimeter(side1, side2, base);
+        if (typeof (myIsocelesTrianglePerimeter) === "number") {
+            document.getElementById("modal-content").innerHTML = `The perimeter of the triangle is ${myIsocelesTrianglePerimeter}.`;
+        } else {
+            document.getElementById("modal-content").innerHTML = myIsocelesTrianglePerimeter;
+        }
+    } else if (triangleType === "equilateral") {
+        let side = document.getElementById('side').value;
+        let myEquilateralTrianglePerimeter = equilateralTrianglePerimeter(side);
+        document.getElementById("modal-content").innerHTML = `The perimeter of the triangle is ${myEquilateralTrianglePerimeter}.`;
+    }
+    modal.showModal();
+}
+
+//It is necessary to call the function to show when the page loads for the first time
+activateSquareForm();
+
 //Function to close the modal
 closeModal.onclick = () => {
     modal.close();
 }
 
-//Exposing the functions for the browser knows about them
+//Exposing the functions for the browser knows about them, since the module is not exposed
 window.calculateSquareArea = calculateSquareArea;
 window.calculateSquarePerimeter = calculateSquarePerimeter;
 window.calculateTrianglePerimeter = calculateTrianglePerimeter;
 window.calculateTriangleArea = calculateTriangleArea;
+window.calculateTriangleHeight = calculateTriangleHeight;
 window.calculateCircleArea = calculateCircleArea;
 window.calculateCirclePerimeter = calculateCirclePerimeter;
 window.activateSquareForm = activateSquareForm;
 window.activateTriangleForm = activateTriangleForm;
 window.activateCircleForm = activateCircleForm;
+window.activateTriangleCalculation = activateTriangleCalculation;
+window.activateTriangleInputs = activateTriangleInputs;
 
--- modules/geometry.js
+++ modules/geometry.js
@@ -7,12 +7,33 @@ const squareArea = (side) => {
     return side * side;
 }
 
-const trianglePerimeter = (side1, side2, base) => {
-    return side1 + side2 + base;
+const equilateralTrianglePerimeter = (side1) => {
+    debugger;
+    return side1 * 3;
 }
 
-const triangleArea = (height, base) => {
+const equilateralTriangleArea = (height, base) => {
     return (base * height) / 2;
+}
+
+//Calculate the heigth of an equilateral triangle
+const equilateralTriangleHeight = (anyside) => {
+    return twoDecimalPlaces((Math.sqrt(3) / 2) * anyside);
+}
+
+//Calculate the area of a isoceles triangle
+const isocelesTriangleArea = (height, base) => {
+    return Math.sqrt((height * height) + (base * base) / 4);
+}
+
+
+const isocelesTriangleHeight = (base, side1, side2) => {
+    if (side1 === side2 && side1 !== base ) {
+            let height = Math.sqrt((4 * side1 * side1) - (base * base)) / 2;
+            return `The height of the isoceles triangle is ${height}.`;
+    } else {
+        return "You enter a wrong value, please try again.";
+    }
 }
 
 const circleDiameter = (radius) => {
@@ -34,4 +55,25 @@ const twoDecimalPlaces = (number) => {
     return Math.round(number * 100) / 100;
 }
 
-export { squareArea, squarePerimeter, triangleArea, trianglePerimeter, circleArea, circlePerimeter };
+const isocelesTrianglePerimeter = (side1, side2, base) => {
+    if (side1 === side2) {
+        let perimeter = base + side1 + side2;
+        return `The perimeter of the isoceles triangle is ${perimeter}.`;
+    } else {
+        return "The sides of the triangle must be equal.";
+    }
+}
+
+
+export {
+    squareArea,
+    squarePerimeter,
+    equilateralTriangleArea,
+    equilateralTrianglePerimeter,
+    isocelesTrianglePerimeter,
+    circleArea,
+    circlePerimeter,
+    isocelesTriangleHeight,
+    isocelesTriangleArea,
+    equilateralTriangleHeight
+};
